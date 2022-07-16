27c8b96e5df575c9da0d024045b02bc6d9954643
wilfred <wolfdog.singini@gmail.com>
2022-07-16 14:32:08 +0200

    7/added five cards/want to add next button

--- scripts/carousel-script.js
+++ scripts/carousel-script.js
@@ -1,28 +1,31 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+showSlides();
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+function showSlides() {    
+  var i;    
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex> slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 6000); // Change image every 5 seconds
+}
+
+function currentSlide(no) {
+  var i;    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slideIndex = no;
+  slides[no-1].style.display = "block";
+}
+
+function plusSlides(n) {
+var newslideIndex = slideIndex + n;
+if(newslideIndex < 4 && newslideIndex > 0){
+   currentSlide(newslideIndex);
+}
 }
