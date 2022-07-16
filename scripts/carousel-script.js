0ff0464b872a55be4f6f1532878935d9a2ab4089
wilfred <wolfdog.singini@gmail.com>
2022-07-16 18:48:46 +0200

    8/bug fixes of carousel

--- scripts/carousel-script.js
+++ scripts/carousel-script.js
@@ -33,39 +33,41 @@ if(newslideIndex < 4 && newslideIndex > 0){
 
 
 // The python scroll
-var slideIndex = 0;
-var slides = document.getElementsByClassName("mySlides2");
+var slideIndex2 = 0;
+var slides2 = document.getElementsByClassName("mySlides2");
 
 showSlides2();
 
 function showSlides2() {    
-  var i;    
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  var k;    
+  for (k = 0; k < slides2.length; k++) {
+      slides2[k].style.display = "none"; 
   }
-  slideIndex++;
-  if (slideIndex> slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
+  slideIndex2++;
+  if (slideIndex2> slides2.length) {slideIndex2 = 1} 
+  slides2[slideIndex2-1].style.display = "block"; 
   setTimeout(showSlides2, 6000); // Change image every 5 seconds
 }
 
-function currentSlide2(no) {
-  var i;    
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+function currentSlide2(no2) {
+  var k;    
+  for (k = 0; k < slides2.length; k++) {
+      slides2[k].style.display = "none"; 
   }
-  slideIndex = no;
-  slides[no-1].style.display = "block";
+  slideIndex2 = no2;
+  slides2[no2-1].style.display = "block";
 }
 
-function plusSlides2(n) {
-var newslideIndex = slideIndex + n;
-if(newslideIndex < 4 && newslideIndex > 0){
-   currentSlide2(newslideIndex);
+function plusSlides2(m) {
+var newslideIndex2 = slideIndex2 + m;
+if(newslideIndex2 < 4 && newslideIndex2 > 0){
+   currentSlide2(newslideIndex2);
 }
 }
 
 
+
+// python scroll word
 function showthis(){
   document.getElementById("prev1").style.display = "block";
 }
