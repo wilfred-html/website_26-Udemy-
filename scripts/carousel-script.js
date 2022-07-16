b6d0c39f7efeda9c1a185cefb703da2cade5a9bb
wilfred <wolfdog.singini@gmail.com>
2022-07-16 18:26:41 +0200

    8/hero2-part2 is now carsousel functioned

--- scripts/carousel-script.js
+++ scripts/carousel-script.js
@@ -29,3 +29,57 @@ if(newslideIndex < 4 && newslideIndex > 0){
    currentSlide(newslideIndex);
 }
 }
+
+
+
+// The python scroll
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides2");
+
+showSlides2();
+
+function showSlides2() {    
+  var i;    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex> slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides2, 6000); // Change image every 5 seconds
+}
+
+function currentSlide2(no) {
+  var i;    
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slideIndex = no;
+  slides[no-1].style.display = "block";
+}
+
+function plusSlides2(n) {
+var newslideIndex = slideIndex + n;
+if(newslideIndex < 4 && newslideIndex > 0){
+   currentSlide2(newslideIndex);
+}
+}
+
+
+function showthis(){
+  document.getElementById("prev1").style.display = "block";
+}
+
+function dontshow(){
+  document.getElementById("prev1").style.display = "none";
+}
+
+
+function both() {
+  if(document.getElementById("prev1").style.display == "block"){
+    dontshow()
+  }
+  else {
+    showthis()
+  }
+}
